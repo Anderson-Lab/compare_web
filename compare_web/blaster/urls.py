fc91638471c73b1c0ab541615552f560cff98d7a
Augie Doebling <augustdoebling@gmail.com>
2022-02-08 13:23:15 -0800

    cleaned up endpoints and standardized responses

--- compare_web/blaster/urls.py
+++ compare_web/blaster/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload_file', views.upload, name='upload'),
     path('create_blast_job', views.create_blast_job, name='create_blast_job'),
+    path('check_job_status', views.check_job_status, name='check_job_status'),
 ]
--- compare_web/blaster/views.py
+++ compare_web/blaster/views.py
@@ -1,30 +1,7 @@
 import re
-from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
-from django.shortcuts import render
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import tasks, job
-import json
-
-# def index(request):
-#    tasks.add.apply_async((1, 2))
-
-   # return HttpResponse("Hello, world. I've called a celery task")
-
-@csrf_exempt
-def upload(request):
-    folder='/Users/augiedoebling/Desktop/' 
-    # print(request.body)
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage(location=folder) #defaults to   MEDIA_ROOT  
-        filename = fs.save(myfile.name, myfile)
-        file_url = fs.url(filename)
-        return HttpResponse("uploaded")
-    else:
-         return HttpResponse("no upload")
-
 
 @csrf_exempt
 def create_blast_job(request):
@@ -46,13 +23,25 @@ def create_blast_job(request):
          blast_job.queued_status()
 
       else :
-         return HttpResponseBadRequest("query and hit files were not detected on the post request")
+         return JsonResponse({
+            'success' : False,
+            'job_id' : '',
+            'error_message' : 'query and hit files were not detected on the post request'
+         })
 
 
       # return success and job id
-      return HttpResponse(blast_job.job_id)
+      return JsonResponse({
+            'success' : True,
+            'job_id' : blast_job.job_id,
+            'error_message' : ''
+         })
    except Exception as e:
-      raise
+      return JsonResponse({
+            'success' : False,
+            'job_id' : '',
+            'error_message' : f'An exception was raised - {e.message}'
+         })
 
 
 @csrf_exempt
@@ -62,15 +51,17 @@ def check_job_status(request):
 
       blast_job.read_status()
 
-      return HttpResponse(json.dumps({
+      return JsonResponse({
          'status' : blast_job.status,
          'message' : blast_job.message,
          'complete' : blast_job.complete,
          'success' : blast_job.success,
-      }))
-
+      })
 
    except Exception as e:
-      raise
-
-   
+      return JsonResponse({
+         'status' : "Unknown",
+         'message' : e.message,
+         'complete' : '',
+         'success' : '',
+      })
